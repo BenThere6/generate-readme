3512e915c05a196a177e8a6ea651190117f84809
BenThere <benjaminbirdsall@icloud.com>
2023-08-24 16:40:40 -0600

    finish inquirer prompt

--- index.js
+++ index.js
@@ -7,37 +7,45 @@ inquirer
         {
             type: 'input',
             name: 'title',
-            message: "What is the title of your application? "
+            message: "What is the title of your application?"
         },
         {
             type: 'input',
             name: 'description',
-            message: "Enter a description: "
+            message: "Enter a description:"
         },
         {
             type: 'input',
             name: 'installation',
-            message: "What are the instructions for installation? "
+            message: "What are the instructions for installation?"
         },
         {
             type: 'input',
             name: 'usage',
-            message: "What are the instructions for use? "
+            message: "What are the instructions for use?"
         },
         {
             type: 'list',
             name: 'license',
-            message: "Select a license: ",
+            message: "Select a license:",
             choices: [
-                
+                'MIT License',
+                'GNU General Public License v2',
+                'GNU General Public License v3',
+                'Apache License 2.0',
+                '3-Clause BSD License',
+                '2-Clause BSD License',
+                'Creative Commons License',
+                'Mozilla Public License 2.0',
+                'Unlicense',
+                'Eclipse Public License',
+                'ISC License',
+                'GNU Affero General Public License'
             ]
-        },
+        }
     ]) 
+    .then(answers => {
+        console.log(answers)
+    })
 
-function writeToFile(fileName, data) {}
-
-function init() {
-    const markdown = generateMarkdown.generateMarkdown(data);
-}
-
-init();
+const markdown = generateMarkdown.generateMarkdown(data);
